d566a90830da4bfd3bdde7ab9e61a551607e7248
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2025-01-02 02:53:50 +0530

    Feat: created nodemailer transport object

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const mailSender = require('./config/email-config');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:  true}));
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+
+    try {
+        const response = await mailSender.sendMail({
+            from: "omgudadhe162162@gmail.com",
+            to: "cj132132@gmail.com",
+            subject: "Node mailer check",
+            text: "Yeah! it's working"
+        })
+
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 });
---
+++ src/utils/common/enums.js
@@ -0,0 +1,9 @@
+const TICKET_STATUS = {
+    SUCCESS: 'success',
+    FAILED: 'failed',
+    PENDING: 'pending'
+}
+
+module.exports = {
+    TICKET_STATUS
+}
